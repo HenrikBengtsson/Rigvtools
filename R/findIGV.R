b039e8fbe1dc5a070bbb3a093333a66f19626dd7
hb <hb@aroma-project.org>
2016-03-01 13:59:20 -0800

    Add findIGV()

---
+++ R/findIGV.R
@@ -0,0 +1,45 @@
+#' Locates the igvtools.jar or igv.jar file
+#'
+#' @return The pathname to the igvtools.jar or igv.jar file
+#'
+#' @export
+#' @importFrom utils file_test
+findIGV <- function() {
+  ## Locate igvtools.jar
+  pathname <- Sys.which("igvtools.jar")
+  if (file_test("-f", pathname)) return(pathname)
+
+  path <- Sys.getenv("IGVTOOLS_HOME", NA_character_)
+  if (file_test("-d", path)) {
+    pathname <- file.path(path, "igvtools.jar")
+    if (file_test("-f", pathname)) return(pathname)
+  }
+
+  ## Locate igv.jar
+  pathname <- Sys.which("igv.jar")
+  if (file_test("-f", pathname)) return(pathname)
+
+  path <- Sys.getenv("IGV_HOME", NA_character_)
+  if (file_test("-d", path)) {
+    pathname <- file.path(path, "igv.jar")
+    if (file_test("-f", pathname)) return(pathname)
+  }
+
+  if (.Platform$OS.type == "windows") {
+    path <- file.path(Sys.getenv("PROGRAMFILES"), "BroadInstitute")
+    if (!file_test("-d", path)) {
+      path <- file.path(Sys.getenv("PROGRAMFILES(x86)"), "BroadInstitute")
+    }
+    if (file_test("-d", path)) {
+      dirs <- list.files(path=path, pattern="^IGV_")
+      paths <- file.path(path, dirs)
+      paths <- paths[file_test("-d", paths)]
+      paths <- sort(paths, decreasing=TRUE)
+      path <- paths[1L]
+      pathname <- file.path(path, "igv.jar")
+      if (file_test("-f", pathname)) return(pathname)
+    }
+  }
+
+  NA_character_
+}
